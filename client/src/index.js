e396149633b15abccd7a4e441c3715fcd52b25b9
kreopelle <kreopelle@gmail.com>
2018-11-19 09:59:01 -0500

    Create store and import to provider, connect with thunk middleware

--- client/src/index.js
+++ client/src/index.js
@@ -7,4 +7,13 @@ import { Provider } from 'react-redux';
 import { createStore, applyMiddleware, compose } from 'redux';
 import thunk from 'redux-thunk';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose
+
+let store = createStore(rootReducer, composeEnhancers(applyMiddleware(thunk)));
+
+ReactDOM.render(
+  <Provider store={store}>
+    <App />
+  </Provider>,
+  document.getElementById('root')
+);
