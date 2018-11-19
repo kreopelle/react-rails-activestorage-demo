d84b6ea45bef302e5dd376144d67ab87590b723c
kreopelle <kreopelle@gmail.com>
2018-11-19 09:54:54 -0500

    Remove service workers

--- client/src/index.js
+++ client/src/index.js
@@ -2,5 +2,9 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
+import rootReducer from './rootReducer'
+import { Provider } from 'react-redux';
+import { createStore, applyMiddleware, compose } from 'redux';
+import thunk from 'redux-thunk';
 
 ReactDOM.render(<App />, document.getElementById('root'));
