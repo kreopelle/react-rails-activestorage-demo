540229dac41858a87cc62b3e3f7ee4cd0da965ee
kreopelle <kreopelle@gmail.com>
2018-11-19 12:17:24 -0500

    Write addPost function to submit new post to database

---
+++ client/src/actions.js
@@ -0,0 +1,16 @@
+export function addPost(post){
+  return (dispatch) => {
+    dispatch({ type: 'START_ADD_POST_REQUEST' })
+    const postData = new FormData()
+    postData.append("post[title]", post.post.title)
+    postData.append("post[description]", post.post.description)
+    postData.append("post[file]", post.post.file)
+    return fetch('/api/posts', {
+      method: 'POST',
+      body: postData,
+      contentType: false,
+    })
+    .then(resp => resp.json())
+    .then(post => dispatch({ type: 'ADD_POST', post }))
+  }
+}
