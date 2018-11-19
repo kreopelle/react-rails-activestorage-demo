b701e436e80ed3ccd466abb3a444bdddd668273e
kreopelle <kreopelle@gmail.com>
2018-11-19 14:55:50 -0500

    Add file_url to list of attributes to serializer

--- app/serializers/post_serializer.rb
+++ app/serializers/post_serializer.rb
@@ -1,7 +1,7 @@
 class PostSerializer < ActiveModel::Serializer
   include Rails.application.routes.url_helpers
 
-  attributes :id, :title, :body
+  attributes :id, :title, :body, :file_url
 
   def file_url
     return rails_blob_url(object.file)
