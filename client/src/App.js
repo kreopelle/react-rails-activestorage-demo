7eef28c4d0cf18da9d7217e4a7a605c75253b83a
kreopelle <kreopelle@gmail.com>
2018-11-19 22:12:15 -0500

    Import getPosts action, connect function, and Posts component

--- client/src/App.js
+++ client/src/App.js
@@ -1,6 +1,9 @@
 import React, { Component } from 'react';
 import './App.css';
 import FormContainer from './FormContainer.js'
+import { getPosts } from './actions.js'
+import { connect } from 'react-redux'
+import Posts from './Posts.js'
 
 class App extends Component {
   render() {
