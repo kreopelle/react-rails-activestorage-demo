33f0867e28d01fc6a2c3395dafcf12ddfd5cf971
kreopelle <kreopelle@gmail.com>
2018-11-19 22:20:32 -0500

    Iterate through posts to render individual posts on page

--- client/src/Posts.js
+++ client/src/Posts.js
@@ -1,8 +1,14 @@
 import React from 'react'
 
 const Posts = (props) => {
+
+  const renderPosts = this.props.posts.map(post => {
+    <li key={post.id}><strong>{post.title}</strong> - {post.body} - {post.file_url}</li>
+  })
+
   return(
     <div className="posts">
+    {this.renderPosts}
     </div>
   )
 }
