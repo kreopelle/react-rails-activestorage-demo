d56e28b948cb7b7d158f31a0c5d8c6c9e1181dd6
kreopelle <kreopelle@gmail.com>
2018-11-20 09:56:12 -0500

    Render posts from GET request on page

--- client/src/Posts.js
+++ client/src/Posts.js
@@ -2,13 +2,11 @@ import React from 'react'
 
 const Posts = (props) => {
 
-  const renderPosts = this.props.posts.map(post => {
-    <li key={post.id}><strong>{post.title}</strong> - {post.body} - {post.file_url}</li>
-  })
-
   return(
     <div className="posts">
-    {this.renderPosts}
+    <ul>
+    {props.posts.map((post) => <li key={post.id}><strong>{post.title}</strong> - {post.body} - {post.file_url}</li>)}
+    </ul>
     </div>
   )
 }
